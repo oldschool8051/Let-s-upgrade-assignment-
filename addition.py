7124be858de5f3437754a9246eec7e6befb74fcd
Divyam Kumar <66730446+oldschool8051@users.noreply.github.com>
2021-02-06 22:21:54 +0530

    Create addition.py

---
+++ addition.py
@@ -0,0 +1,13 @@
+#All Airthmetic operation
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print("Sum is:",(a+b))
+
+if(a>b):
+    print("Substraction is:",(a-b))
+if(b>a):
+    print("Substraction is:",(b-a))
+
+print("Multiplication is:",(a*b))
+
+print("Division is:",(a//b))
